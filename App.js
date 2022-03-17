2fb6a5fd29d4c0106670c1ab2ab7ad6d3c54c9d9
gowshikramr2004 <95086390+gowshikramr2004@users.noreply.github.com>
2022-03-17 14:19:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,Alert,prompt,Image } from 'react-native';
+
+export default function App() {
+  
+  return (
+ 
+    <View style={styles.container}>
+      <Image
+        style={styles.Logo}
+        source={require('./assets/AEPS-Logo.png')}
+      />
+      <Image
+        style={styles.tinyLogo}
+        source={require('./assets/aadhar.png')}
+      />
+   
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  Logo: {
+    bottom:50,
+    width: 264,
+    height: 100,
+  },
+  
+  tinyLogo: {
+    width: 153,
+    height: 133,
+    top:50,
+  },
+});
